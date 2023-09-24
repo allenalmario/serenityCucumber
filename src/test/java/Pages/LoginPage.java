587b10a3dd50667799b0a0fa8c6c7b398b6fffa1
allenalmario <allenalmario@gmail.com>
2023-09-24 15:04:12 -0700

    Alters script to make Serenity manage the webdriver

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -2,18 +2,22 @@ package Pages;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends PageObject {
+public class LoginPage extends PageObject{
 
     @FindBy(id = "user-name")
-    public WebElementFacade user_name_field;
+    WebElement user_name_field;
 
     @FindBy(id = "password")
-    public WebElementFacade password_field;
+    WebElement password_field;
 
     @FindBy(id = "login-button")
-    public WebElementFacade login_button;
+    WebElement login_button;
 
     public void enterUsername(String username) {
 
--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -1,13 +1,14 @@
 package StepDefinitions;
 
 import Pages.LoginPage;
+import Utils.WebDriverManager;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import net.serenitybdd.annotations.Managed;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefinitions {
 
@@ -47,7 +48,7 @@ public class LoginStepDefinitions {
     public void user_should_be_on_products_page_with_the_given_url(String productsPageUrl) {
 
         Assert.assertTrue(driver.getCurrentUrl().equals(productsPageUrl));
-        driver.quit();
+        WebDriverManager.quitDriver();
 
     }
 }
